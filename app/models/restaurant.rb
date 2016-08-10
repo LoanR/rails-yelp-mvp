4a872fa016fa5966cc8538355152daa03c9d0ea4
Loan Robert <loan.rbrt@gmail.com>
2016-08-10 18:15:37 +0200

    probably ugly yelp-like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,15 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  validates :name, uniqueness: true, presence: true
-  validates :address, uniqueness: true, presence: true
-  validates :phone_number, uniqueness: true, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :name, presence: { message: "Add the name of the restaurant" }
+  validates :name, uniqueness: true
+  validates :address, presence: { message: "Add an address" }
+  validates :address, uniqueness: true
+  validates :phone_number, presence: { message: "Add a phone number"}
+  validates :phone_number, uniqueness: true
+  validates :category, presence: { message: "CChoose a category"}
+  validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
+  validates :rating, presence: { message: "Add your review" }
   validates :rating, numericality: true, inclusion: { in: (0..5), allow_nil: false }
-  validates :content, presence: true
+  validates :content, presence: { message: "Choose a rating" }
 
 end
